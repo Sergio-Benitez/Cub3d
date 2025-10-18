9ab58fcef22036510387eadda061e55ea7aff5d0
sergio-benitez <sergio.benitez.ramirez@gmail.com>
2025-10-18 20:34:57 +0200

    New Repo

---
+++ Libft/src/ft_mem/ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenitez <sbenitez@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 20:15:54 by sbenitez          #+#    #+#             */
+/*   Updated: 2025/05/02 17:44:17 by sbenitez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	total_size;
+	void	*ptr;
+
+	total_size = count * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
+	else
+		ft_bzero(ptr, total_size);
+	return (ptr);
+}
